bf249bcfbf44cc62fe219258782092b7d24eb6a0
Akshat Gupta <akshatgupta300@gmail.com>
2023-08-27 22:22:46 +0530

    sorted notes by new ones first

--- src/app/api/auth/[...nextauth]/notesData.js
+++ src/app/api/auth/[...nextauth]/notesData.js
@@ -7,7 +7,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export const GetUserNotes = () => {
   const session = useSession()
 
-  const apiEndpoint = session.status === 'authenticated' ? `http://localhost:5000/api/v1/notes/user/${session.data.user.email}` : null;
+  const apiEndpoint = session.status === 'authenticated' ? `http://localhost:5000/api/v1/notes/user/${session.data.user.email}?sort=-createdAt&api_key=thisisthekey` : null;
   const { data, error, isLoading } = useSWR(apiEndpoint, fetcher, {
     revalidateOnFocus: true, // Revalidate when the tab/window is focused
     revalidateOnReconnect: true, // Revalidate when the network reconnects
