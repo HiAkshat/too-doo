aec13acda6b691a08d81d43498e286165c34996e
Akshat Gupta <akshatgupta300@gmail.com>
2023-08-27 10:50:19 +0530

    added delete functionality to notes

--- src/components/note/note.jsx
+++ src/components/note/note.jsx
@@ -1,10 +1,34 @@
-export default function Note({title, desc}){
-  // console.log(desc)
+import { Toaster } from "@/components/ui/toaster"
+import { useToast } from "@/components/ui/use-toast"
+
+export default function Note({noteId, title, desc}){
+  const { toast } = useToast()
+
+  const handleDelete = async () => {
+    try {
+      const res = await fetch(`http://localhost:5000/api/v1/notes/${noteId}`, {
+        method: "DELETE"
+      })
+
+      console.log("Deleted")
+      toast({
+        title: "Note successfully deleted!",
+      })
+    } catch (err) {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong!",
+      })
+    }
+  }
+
   return (
     <div className="flex flex-col gap-[10px] py-[15px] border border-[#676970] rounded-[10px] whitespace-pre-wrap">
       <span className='text-2xl px-[15px] text-white'>{title}</span>
       <hr className='border-[#676970]'/>
       <p className='px-[15px] text-lg'>{desc}</p>
+      <button onClick={handleDelete}>Delete</button>
+      <Toaster className="dark"/>
     </div>
   )
 }
