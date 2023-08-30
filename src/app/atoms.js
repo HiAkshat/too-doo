177855e1ddbb08b3ffb9138c0a063316b3ddad1e
Akshat Gupta <akshatgupta300@gmail.com>
2023-08-30 10:18:54 +0530

    implemented search functionality

---
+++ src/app/atoms.js
@@ -0,0 +1,5 @@
+import {atom} from "jotai"
+
+const searchInput = ""
+
+export const searchInputAtom = atom(searchInput)
--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,29 @@ import { GetUserNotes, getUserNotes2 } from '@/app/api/auth/[...nextauth]/notesD
 import AddNote from '@/components/addNote/addNote'
 import { useSession } from 'next-auth/react'
 import useSWR from "swr"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Loading from './loading'
+import { searchInputAtom } from './atoms'
+import { useAtom } from 'jotai'
 
 
 export default function Home() {
+  const [searchInput, setSearchInput] = useAtom(searchInputAtom)
+  // const [userNotes, setUserNotes] = useState([])
+  let userNotes;
+
   const session= useSession()
   const {data, error, isLoading} = GetUserNotes()
+
+  const updateUserNotes = () => {
+    const filtered = data.data.notes.filter(
+      note => 
+        note.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+        note.desc.toLowerCase().includes(searchInput.toLowerCase())
+    )
+
+    userNotes=filtered
+  }
 
   if (session.status === 'loading') {
     return <Loading />
@@ -29,7 +45,9 @@ export default function Home() {
     }
 
     // Data is available
-    const userNotes = data.data.notes
+    
+    // const userNotes = data.data.notes
+    updateUserNotes()
     return (
       <main className="">
       <Navbar />
