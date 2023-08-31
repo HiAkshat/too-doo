a9fdab24e7cd4670914b430ac25fabd4c7614d40
Akshat Gupta <akshatgupta300@gmail.com>
2023-08-31 10:21:13 +0530

    removed local storage system added

--- src/app/atoms.js
+++ src/app/atoms.js
@@ -2,9 +2,4 @@ import {atom} from "jotai"
 
 const searchInput = ""
 
-let localNotes
-if (localStorage.getItem("0")===null) localNotes=[]
-else localNotes = JSON.parse(localStorage.getItem("0"))
-
 export const searchInputAtom = atom(searchInput)
-export const localNotesAtom = atom(localNotes)
